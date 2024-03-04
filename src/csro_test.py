10769b5a8a6eb8dd88c889079a092673c80d6f9f
James Heffernan <james.heff@icloud.com>
2024-03-04 18:02:17 -0500

    Register player

---
+++ src/csro_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import rospy
+from csro.srv import RegisterPlayer
+
+if  __name__ == '__main__':
+    rospy.wait_for_service('register_player')
+    register_player = rospy.ServiceProxy('register_player', RegisterPlayer)
+
+    try:
+        resp1 = register_player("test")
+        print(resp1)
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
